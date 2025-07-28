eb4e32af4d6be549f9c52a22405bf39044b28de7
kalitg028 <Amazonbotmv@gmail.com>
2025-07-28 18:40:46 +0530

    Update tera.py

--- plugins/tera.py
+++ plugins/tera.py
@@ -1,4 +1,18 @@
-import os import re import tempfile import requests import asyncio from datetime import datetime, timedelta from urllib.parse import urlencode, urlparse, parse_qs from pyrogram import Client from pyrogram import filters from pyrogram.types import Message from verify_patch import IS_VERIFY, is_verified, build_verification_link, HOW_TO_VERIFY from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton from pymongo import MongoClient import shutil from config import CHANNEL, DATABASE
+import os 
+import re 
+import tempfile 
+import requests 
+import asyncio from datetime 
+import datetime, timedelta from urllib.parse 
+import urlencode, urlparse, parse_qs from pyrogram 
+import Client from pyrogram 
+import filters from pyrogram.types 
+import Message from verify_patch 
+import IS_VERIFY, is_verified, build_verification_link, HOW_TO_VERIFY from pyrogram.types 
+import InlineKeyboardMarkup, InlineKeyboardButton from pymongo 
+import MongoClient 
+import shutil from config 
+import CHANNEL, DATABASE
 
 mongo_client = MongoClient(DATABASE.URI) db = mongo_client[DATABASE.NAME]
 
